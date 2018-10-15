22c6696d9830db8b491a90d762f70d1f17a6a6a3
Kevin Toledo <kgtoledoc@unal.edu.co>
2018-10-15 08:53:35 -0500

    Some changes in code app.js

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const app = express();
 
+const myDB = 'mongodb://localhost/advancedproject1'
+const User = require('./Models/user.model.js');
+const UserRoutes = require('./Controllers/userroutes.js');
+const bodyParser = require('body-parser');
+
 port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}))
+
+mongoose.connect(myDB);
+app.use('/user', UserRoutes);
 
 app.listen(port, (err) => {
   if (!err) {
